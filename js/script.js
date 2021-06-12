a560fe7296bac0cfd65eea38e4765dcd728ca6cb
muaazaltahan <almuaaz559@gmail.com>
2021-06-12 23:11:11 +0300

    Upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var categories = document.getElementById('categories');
+function showCrs(){
+    categories.style.height = '100px';
+    categories.style.width = '100%';
+}
+function hideCrs(){
+    categories.style.height = '0';
+    categories.style.width = '0';
+}
